b1918f4d73a1c2e0e3cfd19680b33ff0984ccf12
berkegit <berkegoktass@hotmail.com>
2023-11-21 17:45:59 +0300

    Add project files.

---
+++ case1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace case1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Birinci sayıyı gir");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İKkinci sayı gir");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = sayi1 + sayi2;
+
+            Console.Write("tekrar sayı gir");
+
+            sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("ikincis sayı gir tekrar");
+
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam2 = sayi1 + sayi2;
+
+            Console.WriteLine("Toplamlari:  " + toplam + "  " + toplam2);
+
+            Console.ReadLine();
+        }
+    }
+}
